6204d053364c6822ef2999e5259bc438293fb5dd
wangwei <wangwei.com@gmail.com>
2012-12-03 16:54:59 +0800

    change serialize src

--- src/main/java/com/hiwayne/commons/serialize/HessianSerialize.java
+++ src/main/java/com/hiwayne/commons/serialize/HessianSerialize.java
@@ -3,18 +3,20 @@ package com.hiwayne.commons.serialize;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.Serializable;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 import com.caucho.hessian.io.Hessian2Input;
 import com.caucho.hessian.io.Hessian2Output;
 import com.caucho.hessian.io.SerializerFactory;
+
 /**
  * 
  * @author wangwei
- *
+ * 
  */
-public class HessianSerialize {
+public class HessianSerialize<T extends Serializable> implements Serializer<T> {
 	private SerializerFactory factory = new SerializerFactory();
 
 	/**
@@ -24,24 +26,38 @@ public class HessianSerialize {
 	 * @return
 	 * @throws IOException
 	 */
-	public byte[] serialize(Object obj) throws IOException {
+	@Override
+	public byte[] serialize(T instance) {
 
 		byte[] reByte;
-		if (obj == null)
+		if (instance == null)
 			throw new NullPointerException();
+		ByteArrayOutputStream os = null;
+		Hessian2Output h2o = null;
+		try {
+			os = new ByteArrayOutputStream();
+			h2o = new Hessian2Output(os);
+			h2o.setSerializerFactory(factory);
+			h2o.writeObject(instance);
+			h2o.flush();
+			reByte = os.toByteArray();
 
-		ByteArrayOutputStream os = new ByteArrayOutputStream();
-		Hessian2Output h2o = new Hessian2Output(os);
-		h2o.setSerializerFactory(factory);
+			return reByte;
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		} finally {
+			if (os != null)
+				try {
+					os.close();
+				} catch (IOException e1) {
+				}
+			if (h2o != null)
+				try {
+					h2o.close();
+				} catch (IOException e) {
+				}
+		}
 
-		h2o.writeObject(obj);
-		h2o.flush();
-		h2o.close();
-
-		reByte = os.toByteArray();
-		os.close();
-
-		return reByte;
 	}
 
 	/**
@@ -51,23 +67,35 @@ public class HessianSerialize {
 	 * @return
 	 * @throws IOException
 	 */
-	public Object deserialize(byte[] by) throws IOException {
-		Object reObj;
-		if (by == null)
+	@Override
+	@SuppressWarnings({ "unchecked" })
+	public T deserialize(byte[] data) {
+		if (data == null)
 			throw new NullPointerException();
-		ByteArrayInputStream is = new ByteArrayInputStream(by);
+		ByteArrayInputStream is = new ByteArrayInputStream(data);
 		Hessian2Input h2i = new Hessian2Input(is);
 		h2i.setSerializerFactory(factory);
-		reObj = h2i.readObject();
-		h2i.close();
-		is.close();
-		return reObj;
+		try {
+			return (T) h2i.readObject();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		} finally {
+			if (h2i != null)
+				try {
+					h2i.close();
+				} catch (IOException e) {
+				}
+			if (is != null)
+				try {
+					is.close();
+				} catch (IOException e) {
+				}
+		}
 	}
 
 	public static void main(String[] arg) {
-
 		long begin = System.currentTimeMillis();
-		HessianSerialize aHessianSerialize = new HessianSerialize();
+		HessianSerialize<Date> aHessianSerialize = new HessianSerialize<Date>();
 		Date old = null;
 		Object newobj = null;
 		for (int i = 0; i < 5; i++) {
@@ -79,7 +107,7 @@ public class HessianSerialize {
 				try {
 					abyte = aHessianSerialize.serialize(old);
 					newobj = aHessianSerialize.deserialize(abyte);
-				} catch (IOException e) {
+				} catch (Exception e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
